f2b42521b9d63e5617ed09d1f8ab74d85c0b5433
regres <colewangyu@gmail.com>
2022-04-03 22:17:31 +0800

    Add mroe themes.

--- src/component/MenuLeft/Theme.js
+++ src/component/MenuLeft/Theme.js
@@ -7,6 +7,7 @@ import {replaceStyle} from "../../utils/helper";
 import TEMPLATE from "../../template/index";
 import "./Theme.css";
 import axios from "axios";
+import localThemeData from "../../json/localThemeList.json";
 
 @inject("content")
 @inject("navbar")
@@ -56,12 +57,13 @@ class Theme extends React.Component {
         }
         remoteThemelist = response.data.data;
       } else {
+        remoteThemelist = localThemeData;
         // 否则默认主题
-        response = await axios.get(THEME_API());
-        if (!response.data.success) {
-          throw new Error();
-        }
-        remoteThemelist = response.data.data.themeList;
+        // response = await axios.get(THEME_API());
+        // if (!response.data.success) {
+        //   throw new Error();
+        // }
+        // remoteThemelist = response.data.data.themeList;
       }
 
       themeList = [
